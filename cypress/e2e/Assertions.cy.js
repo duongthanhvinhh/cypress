782a49650497d24f7fa9f68ea6fea0ac618814d1
duongvinh1706@gmail.com <duongvinh1706@gmail.com>
2023-01-03 15:21:58 +0700

    none

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -1,5 +1,9 @@
-describe("Assertions demo", () => {
-    it("Implicit Assertions"), ()=> {
-        cy.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    }
+describe('Assertions demo',() => {
+    it("Implicit_Assertions", () => {
+        cy.visit("https://automationexercise.com/products")
+        cy.url().should('include','automationexercise.com')
+        cy.url().should('eq','https://automationexercise.com/products')
+        cy.url().should('contain','automationexercise') //should-contain and should-include are the same way
+    })
+    
 })

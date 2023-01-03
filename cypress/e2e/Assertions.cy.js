95bf5bece58d1cc0477a96e2b83ff29fd5fafd5f
duongvinh1706@gmail.com <duongvinh1706@gmail.com>
2023-01-03 17:03:58 +0700

    none

--- cypress/e2e/Assertions.cy.js
+++ cypress/e2e/Assertions.cy.js
@@ -1,9 +1,46 @@
 describe('Assertions demo',() => {
-    it("Implicit_Assertions", () => {
+    it("Implicit_Assertions Example #1", () => {
         cy.visit("https://automationexercise.com/products")
+
         cy.url().should('include','automationexercise.com')
         cy.url().should('eq','https://automationexercise.com/products')
         cy.url().should('contain','automationexercise') //should-contain and should-include are the same way
-    })
+
+        //all 3 assertions above can be group by only one line as below:
+        cy.url().should('contain','automationexercise')
+                .and('eq','https://automationexercise.com/products')
+                .and('contain','automation')
+                .and('not.contain','vinhtester')
+
+
+        cy.title().should('eq','Automation Exercise - All Products')
+
+        cy.get('#search_product').should('be.visible')
+        cy.get('#search_product').should('exist')
     
+    })
+
+    it("Implicit_Assertions Example #2", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+        cy.get('.oxd-button').should('be.visible')
+        cy.get('.oxd-button').should('exist')
+
+        cy.get("input[placeholder='Username']").type("Admin")
+        cy.get("input[placeholder='Password']").type("admin123")
+        cy.get("button[type='submit']").click()
+    }) 
+
+    it("Explicit_Assertions Example #1", () => {
+        cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+
+        cy.get('.oxd-button').should('be.visible')
+        cy.get('.oxd-button').should('exist')
+
+        cy.get("input[placeholder='Username']").type("Admin")
+        cy.get("input[placeholder='Password']").type("admin123")
+        cy.get("button[type='submit']").click()
+
+        let expName='abc';
+    })
 })

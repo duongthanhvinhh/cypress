3c54a596dd1be2720f31d223b9a6d2b734ab844c
duongvinh1706@gmail.com <duongvinh1706@gmail.com>
2023-01-05 09:19:58 +0700

    adding script: interact with checkbox

--- cypress/e2e/RadioButtonsAndCheckboxes.cy.js
+++ cypress/e2e/RadioButtonsAndCheckboxes.cy.js
@@ -1,5 +1,5 @@
 describe("check UI Element", ()=>{
-
+/*
     it("Checking Radio Buttons",()=>{
 
         cy.visit("https://itera-qa.azurewebsites.net/home/automation")
@@ -10,6 +10,33 @@ describe("check UI Element", ()=>{
         
         //selecting radio buttons
         cy.get("input#male").check().should('be.checked')
+        cy.get("input#female").should('not.be.checked')
+    })
+*/
+
+    it("Checking Checkboxes",()=>{
+
+        cy.visit("https://itera-qa.azurewebsites.net/home/automation")
+
+        //Visibility of the element
+        cy.get('input#monday').should('be.visible')
+
+        //Selecting single checkbox - Monday
+        cy.get("input#monday").check().should('be.checked')
+
+        //Unselecting checkbox
+        cy.get("input#monday").uncheck().should('not.be.checked')
+
+        //Selecting all the checkboxes
+        cy.get("input.form-check-input[type='checkbox']").check().should('be.checked')
+
+        //Unselecting all the checkboxes
+        cy.get("input.form-check-input[type='checkbox']").uncheck().should('not.be.checked')
+
+        //Selecting fist and last checkbox
+        cy.get("input.form-check-input[type='checkbox']").first().check().should('be.checked')
+        cy.get("input.form-check-input[type='checkbox']").last().check().should('be.checked')
+
     })
 
 })
